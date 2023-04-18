6d22dec064cddf1bb7d8c3606c30d34f6d8628d4
sajjad arghaee <84894722+Sajjad-Arghaee@users.noreply.github.com>
2023-04-18 15:25:16 +0330

    Update main.c

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -22,6 +22,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "string.h"
+#include <stdlib.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -447,46 +448,58 @@ void decode(int num, int BCD[4]) {
 
 
 int compare(char *str1, char *str2) {
+    int str1_length = strlen(str1);
+    int str2_length = strlen(str2);
     char *words1[200];
     char *words2[200];
     char *word;
-    int i, j, count1 = 0, count2 = 0, similar_words = 0, similar1 = 0;
-
+    int i, j, count1 = 0, count2 = 0, similar1 = 0;
+    float similar_words = 0;
     // Split string1 into words
-    word = strtok(str1, " ");
+    word = strtok(str1, " \n");
     while (word != NULL && count1 < 200) {
         words1[count1++] = word;
-        word = strtok(NULL, " ");
+        word = strtok(NULL, " \n");
     }
 
     // Split string2 into words
-    word = strtok(str2, " ");
+    word = strtok(str2, " \n");
     while (word != NULL && count2 < 200) {
         words2[count2++] = word;
-        word = strtok(NULL, " ");
+        word = strtok(NULL, " \n");
     }
-
+    
+    long int index_1 = 0;
+    long int index_2 = 0;
     // Compare the words in both strings
     for (i = 0; i < count1; i++) {
+        index_2 = 0;
         for (j = 0; j < count2; j++) {
             if (strcmp(words1[i], words2[j]) == 0) {
-                similar_words++;
+                similar_words += 1.0 - abs(index_2 - index_1) / ((str1_length + str2_length) / 2.0);
+                printf("for word : %s index1 is : %ld\n", words1[i], index_1);
+                printf("index2 is : %ld\n", index_2);
                 break;
             }
+            index_2 += strlen(words2[j]);
         }
+        index_1 += strlen(words1[i]);
     }
-    for (i = 0; i < count1; i++){
-    	if(strstr(str2, words1[i]) != NULL){
-    		similar1++;
-    	}
-    }
+    // for (i = 0; i < count1; i++){
+    // 	if(strstr(str2, words1[i]) != NULL){
+    // 		similar1++;
+    // 	}
+    // }
+    // hello new world
+    // world counter hi hello
 
     // Calculate similarity percentage
     if (count1 == 0 || count2 == 0) {
         return 0;
     } else {
-        int similarity = (similar1 * 100) / ((count1 + count2) / 2);
-        return similarity;
+        float similarity = (similar_words * 100.0) / ((count1 + count2) / 2.0);
+        similarity += 0.51;
+        return (int)similarity;
     }
 }
 
